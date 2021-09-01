ce5dabdd682804a5dad8c2bb939a8def163abc9c
rpire <rubenpire7@gmail.com>
2021-09-01 18:10:04 -0400

    Remove commented lines from add_new.js

--- src/add_new.js
+++ src/add_new.js
@@ -1,24 +1,4 @@
 const addNew = async (name, points) => {
-  // const leaderboardItem = document.createElement('li');
-  // leaderboardItem.classList.add('flex-box', 'item');
-  // if (num % 2 === 0) {
-  //   leaderboardItem.classList.add('even');
-  // } else {
-  //   leaderboardItem.classList.add('odd');
-  // }
-
-  // const nameContainer = document.createElement('span');
-  // nameContainer.classList.add('item-player');
-  // nameContainer.innerHTML = name;
-
-  // const scoreContainer = document.createElement('span');
-  // scoreContainer.classList.add('item-score');
-  // scoreContainer.innerHTML = score;
-
-  // leaderboardItem.appendChild(nameContainer);
-  // leaderboardItem.appendChild(scoreContainer);
-
-  // leaderboard.appendChild(leaderboardItem);
   await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1NoC71hDswXGQoJ5wACA/scores/', {
     method: 'POST',
     body: JSON.stringify({
